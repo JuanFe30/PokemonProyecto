641bdaba3cf6682c14f4f3c84f8abecdd2f80d7d
JuanFe30 <j_f_garzon@hotmail.com>
2022-06-30 22:02:07 -0500

    master-js

--- index.php
+++ index.php
@@ -1,39 +1,8 @@
 <?php include "head.php" ?>
-    <h1>Hola este es el virtual host</h1>
-
-    <div class="container">
-        <div class="col-md-12 row">
-            <div class="col-md-4">
-                        <div class="card" style="width: 18rem;">
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/crystal/back/shiny/132.png" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-                </div>
-            </div>
-
-            <div class="col-md-4">
-            <div class="card" style="width: 18rem;">
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/crystal/back/shiny/132.png" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-            <div class="card" style="width: 18rem;">
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/crystal/back/shiny/132.png" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-                </div>
-            </div>
-        </div>
+<div class="container">
+    <div class="col-md-12 row" id="resultados">
+         
     </div>
+</div>
+<script src="master.js"></script>
 <?php include "footer.php" ?>
